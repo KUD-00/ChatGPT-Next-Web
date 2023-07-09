3c5cdf517612fea407208b1d89d1a3da485c0d8f
KUD <kasa7qi@gmail.com>
2023-07-09 13:11:01 +0900

    try to add captcha support

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -3,20 +3,32 @@
 import styles from "./auth.module.scss";
 import { IconButton } from "./button";
 
+import { fetchEventSource } from "@fortaine/fetch-event-source";
+
 import { useNavigate } from "react-router-dom";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Path } from "../constant";
 import { useAccessStore } from "../store";
 import Locale from "../locales";
 
 import BotIcon from "../icons/bot.svg";
 
+import Image from "next/image";
+
+interface CaptchaData {
+  captcha_id: string;
+  captcha: string;
+}
+
 export function AuthPage() {
   const navigate = useNavigate();
   const access = useAccessStore();
-  const [isRegister, setIsRegister] = useState(false);
+  const [isRegister, setIsRegister] = useState<boolean>(false);
+  const [captchaData, setCaptchaData] = useState<CaptchaData | null>(null);
+
   const accountRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
+  const verifyRef = useRef<HTMLInputElement | null>(null);
 
   const goHome = () => navigate(Path.Home);
 
@@ -30,14 +42,53 @@ export function AuthPage() {
 
   const handleRegisterClick = () => {};
 
+  useEffect(() => {
+    if (isRegister) {
+      fetch("https://bot100.app:7001/api/v1/captcha", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setCaptchaData(data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      /* fetchEventSource('https://bot100.app:7001/api/v1/captcha', {
+       *   method: 'GET',
+       *   headers: {
+       *     'Access-Control-Allow-Origin': 'http://localhost:3000',
+       *   },
+       *   onmessage(ev) {
+       *     console.log(ev.data);
+       *   }
+       * }) */
+    }
+  }, [isRegister]);
+
   return (
     <div className={styles["auth-page"]}>
       <div className={`no-dark ${styles["auth-logo"]}`}>
         <BotIcon />
       </div>
 
-      {isRegister ? (
-        <div className={styles["auth-title"]}>{Locale.Auth.Register}</div>
+      {isRegister && captchaData ? (
+        <>
+          <div className={styles["auth-title"]}>{Locale.Auth.Register}</div>
+          <Image src={captchaData.captcha} alt="verification-code" />
+
+          <input
+            id="verification"
+            className={styles["auth-input"]}
+            type="text"
+            ref={verifyRef}
+            placeholder={Locale.Auth.Account}
+          />
+        </>
       ) : (
         <div className={styles["auth-title"]}>{Locale.Auth.Title}</div>
       )}
